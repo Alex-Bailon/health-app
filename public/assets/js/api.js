238e89438b3e123e055d994378861087e2803188
rodriguezh21 <rodriguezh21@gmail.com>
2020-05-11 21:36:04 -0500

    added search form to main

---
+++ public/assets/js/api.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function () {
+$("#foodform").on("submit", function(event){
+event.preventDefault()
+var foodSearch = $("#foodSearch").val().trim()
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://edamam-edamam-nutrition-analysis.p.rapidapi.com/api/nutrition-data?ingr=" + foodSearch,
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "edamam-edamam-nutrition-analysis.p.rapidapi.com",
+      "x-rapidapi-key": "7414a69555mshdafeae439feb5b5p14f0dejsn72987a0fcc7a"
+    }
+  }
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
+})
+  })
