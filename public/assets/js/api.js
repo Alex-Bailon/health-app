83db295e88af364289488fd4d5bac1230cdf399c
Alex B <abailon949@gmail.com>
2020-05-13 19:30:13 -0700

    added color to charts

--- public/assets/js/api.js
+++ public/assets/js/api.js
@@ -15,16 +15,20 @@ $(document).ready(function () {
       $('#calChart').text(data.calories)
       // console.log(data)
 
-      function makeChart(data, max, color) {
+      function makeChart (data, max, color) {
         return {
           type: 'doughnut',
           data: {
             datasets: [
               {
                 data: [data, max - data],
-                backgroundColor: color
+                backgroundColor: [color, 'lightgray']
               }
-            ]
+            ],
+            labels: [
+              'This Item',
+              'Recommended',
+          ]
           },
           options: {}
         }
@@ -33,16 +37,23 @@ $(document).ready(function () {
       const ctx = $('#calChart')
       const myCalChart = new Chart(ctx, makeChart(data.calories, 2000, 'red'))
 
-
       const ctx2 = $('#fatChart')
-      const myFatChart = new Chart(ctx2, makeChart(data.totalNutrients.FAT.quantity.toFixed(2), 77, 'green'))
-
+      const myFatChart = new Chart(
+        ctx2,
+        makeChart(data.totalNutrients.FAT.quantity.toFixed(2), 77, 'green')
+      )
 
       const ctx3 = $('#sugarChart')
-      const mySugarChart = new Chart(ctx3, makeChart(data.totalNutrients.SUGAR.quantity.toFixed(2), 37, 'ornage'))
+      const mySugarChart = new Chart(
+        ctx3,
+        makeChart(data.totalNutrients.SUGAR.quantity.toFixed(2), 37, 'orange')
+      )
 
       const ctx4 = $('#carbChart')
-      const myCarbChart = new Chart(ctx4, makeChart(data.totalNutrients.CHOCDF.quantity.toFixed(2), 275, 'blue'))
+      const myCarbChart = new Chart(
+        ctx4,
+        makeChart(data.totalNutrients.CHOCDF.quantity.toFixed(2), 275, 'blue')
+      )
     })
   })
 })
