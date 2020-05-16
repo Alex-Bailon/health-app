1d31b25cf3602eee1a923b703c0f33f4ea96a541
Alex B <abailon949@gmail.com>
2020-05-16 08:38:32 -0700

    added comments to backend code

--- controllers/appController.js
+++ controllers/appController.js
@@ -3,28 +3,26 @@ const router = express.Router()
 const db = require('../models')
 const passport = require('../config/passport')
 const bcrypt = require('bcryptjs')
-const axios = require("axios");
+const axios = require('axios')
 const {
   checkAuth,
   forwardAuth
 } = require('../config/middleware/isAuthenticated')
+// get request when going to homepage with middleware to check if user is already logged in. If so forwards them to /userhome
 router.get('/', forwardAuth, function (req, res) {
   res.render('index')
 })
-
+// get request to /userhome with middleware to ensure user is logged in.
 router.get('/userhome', checkAuth, function (req, res) {
   res.render('userHome', {
     user: req.user
   })
 })
-
-router.post('/api/login', passport.authenticate('local'), function(req, res) {
+// post request when user wants to login it will run middleware to check if everything matches
+router.post('/api/login', passport.authenticate('local'), function (req, res) {
   res.json(req.user)
 })
-
-  // return res.json(req.user);
-
-
+// post request for when the user wants to signup. Password is hashed.
 router.post('/api/signup', function (req, res) {
   let { username, email, password } = req.body
   bcrypt.genSalt(10, (err, salt) => {
@@ -46,41 +44,33 @@ router.post('/api/signup', function (req, res) {
     })
   })
 })
-
-router.post("/api/food", function(req,res){
-  // get data from edamam
-  // get user id from req.headers
-  // add data from edamam to food item table
-  // join table user_food 
-  // get all food for user
-  // send it back
-  // possibly add controllers for different files i.e. user_food
-  console.log("This works")
-  console.log(req.headers["x-user-id"])
+// post request for when user looks up a food.
+router.post('/api/food', function (req, res) {
   axios({
-    "method":"GET",
-    "url":"https://edamam-edamam-nutrition-analysis.p.rapidapi.com/api/nutrition-data",
-    "headers":{
-    "content-type":"application/octet-stream",
-    "x-rapidapi-host":"edamam-edamam-nutrition-analysis.p.rapidapi.com",
-    "x-rapidapi-key":"7414a69555mshdafeae439feb5b5p14f0dejsn72987a0fcc7a",
-    "useQueryString":true
-    },"params":{
-    "ingr":req.body.item
+    method: 'GET',
+    url:
+      'https://edamam-edamam-nutrition-analysis.p.rapidapi.com/api/nutrition-data',
+    headers: {
+      'content-type': 'application/octet-stream',
+      'x-rapidapi-host': 'edamam-edamam-nutrition-analysis.p.rapidapi.com',
+      'x-rapidapi-key': '7414a69555mshdafeae439feb5b5p14f0dejsn72987a0fcc7a',
+      useQueryString: true
+    },
+    params: {
+      ingr: req.body.item
     }
-    })
-    .then((response)=>{
-     
+  })
+    .then(response => {
       res.json(response.data)
     })
-    .catch((error)=>{
+    .catch(error => {
       console.log(error)
-    });
-  })
-
+    })
+})
+// get request to log user out
 router.get('/logout', function (req, res) {
   req.logout()
   res.redirect('/')
-});
+})
 
 module.exports = router
